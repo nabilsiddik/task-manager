088b17677681feedfa78fb3f81efb2c892c15cae
nabilsiddik <nabilsiddik90@gmail.com>
2025-05-01 22:34:54 +0600

    last commit from nabil siddik

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:task_manager/app/app.dart';
-
+// main 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences preferences = await SharedPreferences.getInstance();
